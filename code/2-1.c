26f44079002ec3345054f9f7fbdd0cce83ab2c36
gyeon24 <162951845+gyeon24@users.noreply.github.com>
2026-04-24 09:24:15 +0900

    Update 2-1.c

--- code/2-1.c
+++ code/2-1.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-void foo(){
-    char *str1 = "hello";
-    printf("%p\n", (void*)str1);
-}
-void bar(){
-    char *str2 = "hello";
-    printf("%p\n", (void*)str2);
-}
-
 int main() {
-    foo();
-    bar();
+    char arr[] = "hello";
+    char *p = "hello";
+
+    arr[0] = 'H';
+    printf("%s\n", arr);
+
+    // p[0] = 'H'; 
+    printf("%s\n", p);
 
     return 0;
 }
